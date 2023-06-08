403960086c2c1c71a7c7701c6d5062d24f89dff9
Santhosh <santhoshcv2004@gmail.com>
2023-06-09 00:38:32 +0530

    Added detail page and removed resume and insta

--- all_reviews/urls.py
+++ all_reviews/urls.py
@@ -5,4 +5,5 @@ app_name='all_reviews'
 
 urlpatterns = [
     path('',views.all, name='all'),
+    path('<int:alls_id>/',views.detail, name='detail'),
 ]
--- all_reviews/views.py
+++ all_reviews/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import alls
 
 def all(request):
-    revs=alls.objects.order_by('-title')
+    revs=alls.objects.order_by('title')
     return render(request, 'all_reviews/all.html',{'revs':revs})
+
+def detail(request, alls_id):
+    als=get_object_or_404(alls, pk=alls_id)
+    return render(request, 'all_reviews/detail.html',{'als':als})
