a085a8559acb68a4c8013d7ac05334f887cc1d83
Santhosh <santhoshcv2004@gmail.com>
2023-06-11 15:04:04 +0530

    Added link to project title for detail page in both home and all reviews page

--- tvshows/urls.py
+++ tvshows/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from tvshowss import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home'),
